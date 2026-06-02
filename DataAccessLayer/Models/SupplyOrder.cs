72067eff25aeb04caa798ff79754dc9384ae2933
Bram-Starren <2504125starren@zuyd.nl>
2026-06-02 14:07:09 +0200

    Class Complaint en SupplyOrder Toegevoegd

---
+++ DataAccessLayer/Models/SupplyOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class SupplyOrder
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime DateOfDelivery { get; set; }
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
