0d4f71ed79ab71ef29366a53a3fb11ab73c5a0e6
Mik <mik.penders@gmail.com>
2026-06-04 09:34:36 +0200

    - Updated product class

--- DataAccessLayer/Models/Product.cs
+++ DataAccessLayer/Models/Product.cs
@@ -13,9 +13,13 @@ namespace DataAccessLayer.Models
 
         public string Name { get; set; }
 
+        public string ProductCode { get; set; }
+
         public string Description { get; set; }
 
         public decimal Price { get; set; }
+
+        public int Stock { get; set; }
 
         public ICollection<Order> Orders { get; } = new List<Order>();
 
